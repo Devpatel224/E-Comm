d31bb52457d6b3d9c9932627ba2b5ba059ddac10
Dev Patel <devpatel9773@gmail.com>
2024-11-13 21:25:23 +0530

    Create CommonForm , registerFormControls and loginFormControls

---
+++ client/src/config/index.js
@@ -0,0 +1,43 @@
+
+export const registerFormControls = [
+    {
+        name :'username',
+        label : 'User Name',
+        placeholder : 'Enter Your user Name',
+        componentType : 'input',
+        type : 'text',
+    },
+    {
+        name :'email',
+        label : 'Email',
+        placeholder : 'Enter Your user email',
+        componentType : 'input',
+        type : 'email',
+    },
+    {
+        name :'password',
+        label : 'password',
+        placeholder : 'Enter Your password',
+        componentType : 'input',
+        type : 'password',
+    }
+]    
+
+
+
+export const loginFormControls = [
+    {
+        name :'email',
+        label : 'Email',
+        placeholder : 'Enter Your user email',
+        componentType : 'input',
+        type : 'email',
+    },
+    {
+        name :'password',
+        label : 'password',
+        placeholder : 'Enter Your password',
+        componentType : 'input',
+        type : 'password',
+    }
+]    
