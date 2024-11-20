29c00c47c507fb1b6bc7c9aee5238e171c0f08f9
Dev Patel <devpatel9773@gmail.com>
2024-11-20 08:07:13 +0530

    Add addProduc,fetchProducts, editProducts and deleteProducts api

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -1,20 +1,100 @@
 const { imageUploadUtils } = require("../../helpers/cloudinary");
+const {productModel} = require("../../models/Product")
+const {createCustomeError} = require("../../utils/customeError")
 
+const handleImageUpload = async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    const url = "data:" + req.file.mimetype + ";base64," + b64;
+    const result = await imageUploadUtils(url);
 
-const handleImageUpload = async(req,res)=>{
-    try{
-        const b64 = Buffer.from(req.file.buffer).toString('base64');
-        const url = 'data:' + req.file.mimetype + ";base64," + b64;
-        const result = await imageUploadUtils(url);
+    res.json({
+      success: true,
+      result: result,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
 
-        res.json({
-            success : true,
-            result : result
-        })
-    }catch(err){
-        console.log(err)
-        next(err)
+// add a new product
+const addProduct = async (req, res) => {
+  try {
+    const {image, title, description, category, brand, price, salePrice, totalStock} = req.body
+
+      const createdProduct = await productModel({image, title, description, category, brand, price, salePrice, totalStock})
+
+      res.status(201).json({
+        success:true,
+        data:createdProduct
+      })
+  } catch (err) {
+    next(err);
+  }
+};
+
+// fetch all products
+const fetchProducts = async (req, res) => {
+    try {  
+      const listOfProducts = await productModel.find({});
+      res.status(201).json({
+        success:true,
+        data:listOfProducts
+      })
+
+    } catch (err) {
+      next(err);
     }
-}
+  };
+ 
 
-module.exports = {handleImageUpload}
+// edit a product
+const editProduct = async (req, res) => {
+    try {
+      const {image, title, description, category, brand, price, salePrice, totalStock} = req.body
+        const {id} = req.params
+
+        const findProduct = await productModel.findById(id);
+        if(!findProduct) return next(createCustomeError(401,"Product not Found"))
+            
+        findProduct.title = title ||  findProduct.title
+        findProduct.description = description ||  findProduct.description
+        findProduct.category = category ||  findProduct.category
+        findProduct.brand = brand ||  findProduct.brand
+        findProduct.price = price ||  findProduct.price
+        findProduct.salePrice = salePrice ||  findProduct.salePrice
+        findProduct.totalStock = totalStock ||  findProduct.totalStock
+        findProduct.image = image ||  findProduct.image
+
+        await findProduct.save();
+        res.status(200).json({
+            success:true,
+            data : findProduct
+        })
+
+    } catch (err) {
+      next(err);
+    }
+  };
+
+// delete a product
+
+const deleteProduct = async (req, res) => {
+    try {
+      const {id} = req.params
+      const product = await productModel.findByIdAndDelete(id)
+
+      if(!product) return next(createCustomeError(404,"Product not Found"))
+
+       res.status(201).json({
+        success:true,
+        messsage:"Product Deleted successfully"
+       })
+    } catch (err) {
+      next(err);
+    }
+  };
+
+
+module.exports = { handleImageUpload, addProduct, fetchProducts,editProduct,deleteProduct };
--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router()
 
-const {handleImageUpload} = require("../../controllers/admin/products-controller")
+const {handleImageUpload , addProduct,fetchProducts,deleteProduct,editProduct} = require("../../controllers/admin/products-controller")
 const {upload} = require("../../helpers/cloudinary")
 
 
 router.post("/upload-image",upload.single('my_file'),handleImageUpload)
+router.post("/add",addProduct)
+router.put("/edit/:id",editProduct)
+router.delete("/delete/:id",deleteProduct)
+router.get("/get",fetchProducts)
 
 
  module.exports = router
