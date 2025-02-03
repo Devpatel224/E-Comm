25f9e50dd71fe027515d6f3bfdb0b595b231d2d9
Dev Patel <devpatel9773@gmail.com>
2025-02-03 21:31:38 +0530

    Add fetchToCart API

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -39,7 +39,42 @@ const addToCart = async (req, res, next) => {
 };
 const fetchToCart = async (req, res, next) => {
   try {
-    
+    const {userId} = req.body;
+
+    if(!userId){
+        next(createCustomeError(401,"Invalid Data Provided"))
+    }
+
+    const cart = await cart.findOne({userId}).populate({
+        path : 'items.productId',
+        select : 'image title price salePrice'
+    })
+
+    if(!cart) next(createCustomeError(404,"Cart Not Found"))
+
+    const validItems = cart.items.filter(productItem => productItem.productId);
+
+    if(validItems.length < cart.items.length){  // Very important Logic
+        cart.items = validItems;
+        await cart.save()
+    }
+
+    const populateCartItems = validItems.map(item => ({
+        productId : item.productId._id,
+        image : item.productId.image,
+        price : item.productId.price,
+        salePrice : item.productId.salePrice,
+        title : item.productId.title,
+        quantity : item.quantity
+    }))
+
+    res.status(200).json({
+        success:true,
+        data : {
+            ...cart._doc,  // send only raw data exclude methods like save , populate etc from mongoose object
+            items : populateCartItems // send newly created items
+        } 
+    })
 
   } catch (e) {
     next(e);
